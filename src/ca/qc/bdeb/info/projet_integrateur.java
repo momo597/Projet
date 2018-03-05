c98b653c82ae9535ce9614bb083e5586bd52fe22
Mohamed Ayouch <ayouch.mohamed.310@gmail.com>
2018-03-05 11:18:45 -0500

    Initial Commit

--- src/ca/qc/bdeb/info/projet_integrateur.java
+++ src/ca/qc/bdeb/info/projet_integrateur.java
@@ -15,7 +15,7 @@ public class projet_integrateur {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Allo!");
     }
     
 }
