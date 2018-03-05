b995ded666d30bd2985198463853068b96b2472b
Mohamed Ayouch <ayouch.mohamed.310@gmail.com>
2018-03-05 11:17:22 -0500

    Initial Commit

---
+++ src/ca/qc/bdeb/info/projet_integrateur.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.qc.bdeb.info;
+
+/**
+ *
+ * @author 1624013
+ */
+public class projet_integrateur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
