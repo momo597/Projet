a0cdeaa79455802627bd65bc0afa1557a3b64b70
1661223 <1661223@li-h015-0t27>
2018-04-09 12:56:29 -0400

    NPC

---
+++ src/ca/qc/bdeb/info/NPC.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.qc.bdeb.info;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author 1661223
+ */
+public class NPC {
+    String dialogue;
+   
+
+    }
+
